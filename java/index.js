32f3c29f835ba8553e039d0a23a1ff85c1982117
M-Muiz <122803576+M-Muiz@users.noreply.github.com>
2023-03-21 14:59:20 +0500

    chp 26-30

---
+++ java/index.js
@@ -0,0 +1,93 @@
+Q: 1
+
+// let a  = prompt("Enter a positive integer", "3.45214");
+
+// for(var i = 0; i< a.length; i++){
+//     if(a < 0){
+//         alert("Enter a positive number")
+//         break
+//     }
+// }
+
+// let round = Math.round(a);
+// let floor = Math.floor(a);
+// let ceil = Math.ceil(a);
+
+// document.write(`User Input: ${a} <br>`);
+// document.write(`Round off Value: ${round} <br>`);
+// document.write(`Floor Value: ${floor} <br>`);
+// document.write(`Ceil Value: ${ceil} <br>`);
+
+Q: 2 
+
+// let a  = prompt("Enter a negative integer", "-2.673");
+
+// for(var i = 0; i< a.length; i++){
+//     if(a > 0){
+//         alert("Enter a negative number")
+//         break
+//     }
+// }
+//         let round = Math.round(a);
+//         let floor = Math.floor(a);
+//         let ceil = Math.ceil(a);
+        
+//         document.write(`User Input: ${a} <br>`);
+//         document.write(`Round off Value: ${round} <br>`);
+//         document.write(`Floor Value: ${floor} <br>`);
+//         document.write(`Ceil Value: ${ceil} <br>`);
+    
+Q: 3
+
+// let a = prompt("Enter a  number", "-4");
+
+// let abs = Math.abs(a);
+
+// document.write(`The absolute value of ${a} is ${abs}`);
+
+Q:4
+
+let random = Math.floor(Math.random() * 6 +1);
+// console.log(random)
+
+document.getElementById("btn").onclick = function(){
+    document.getElementById("lbl").innerHTML = `Random dice Value = ${random}`;
+}
+
+Q:5
+
+// let toss = prompt("Choose head & tail");
+
+// toss = toss.toLowerCase();
+
+// let random = Math.floor(Math.random());
+
+// if(toss == "head"){
+//     alert("You win the toss")
+// }else if(toss == "tail"){
+//     alert("You loss the toss")
+// };
+
+
+Q:6
+
+// let a = Math.floor(Math.random() * 100);
+
+// document.write(`Random number between 1 and 100: ${a}`);
+
+Q:7
+// let weight = +prompt("Enter your weight");
+
+// document.write(`Your weight is: ${weight} Kilograms`);
+
+
+Q:8
+
+// let guess = Math.floor(Math.random() * 100 + 1);
+// console.log(guess)
+// let num = prompt("Guess a number between 1 to 100");
+// num =  Number(num);
+//     if(num == guess){
+//         alert("Congrats! You Win")}
+    
+    
